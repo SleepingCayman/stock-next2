2c7032d32b21b8ca96d98d0e3b6024fbdaefd1ed
SleepingCayman <chenhouzhen@foxmail.com>
2023-02-19 11:36:54 +0700

    mongoDB

---
+++ pages/api/stock/products/index.js
@@ -0,0 +1,32 @@
+import {connect, model, models, Schema} from "mongoose"
+const connectionString = 'mongodb+srv://user1:qqqqq123@cluster0.gkl5jni.mongodb.net/stock'
+
+
+export default async function handler(req, res) {
+    await connect(connectionString);
+    console.log("req.method: ", req.method)
+
+    if (req.method === 'GET') {
+        const docs = await Article.find()
+        res.status(200).json(docs)
+    } else if (req.method === 'POST') {
+        console.log(req.body)
+        // res.status(200).json(req.body)
+        const doc = await Article.create(req.body)
+        res.status(201).json(doc)
+    } else {
+        res.setHeader('Allow', ['GET', 'POST'])
+        res.status(405).end(`Method ${req.method} Not Allowed`)
+    }
+}
+
+
+
+const articleSchema = new Schema({
+    code: String,
+    name: String,
+    price: Number,
+});
+
+console.log("Mongoose Models", models)
+const Article = models?.article || model('article', articleSchema);
