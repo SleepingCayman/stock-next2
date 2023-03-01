3a1d84d61bd01b3366712edc1c015a302b071ca8
SleepingCayman <chenhouzhen@foxmail.com>
2023-03-01 15:58:29 +0700

    666

--- pages/api/blogs/articles/index.js
+++ pages/api/blogs/articles/index.js
@@ -10,9 +10,9 @@ export default async function handler(req, res) {
         res.status(200).json(docs)
     } else if (req.method === 'POST') {
         console.log(req.body)
-        res.status(200).json(req.body)
-        // const doc = await Article.create(req.body)
-        // res.status(201).json(doc)
+        //res.status(200).json(req.body)
+        const doc = await Article.create(req.body)
+        res.status(201).json(doc)
     } else {
         res.setHeader('Allow', ['GET', 'POST'])
         res.status(405).end(`Method ${req.method} Not Allowed`)
