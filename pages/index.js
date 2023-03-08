51ea406f9fca6576bc3dade21badc89645fc01e9
SleepingCayman <chenhouzhen@foxmail.com>
2023-03-08 15:41:59 +0700

    OK

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,20 @@
-import Head from 'next/head'
-import Link from 'next/link'
-
+import Head from "next/head";
+import Link from "next/link";
+import LoginButton from "../components/login-btn";
 export default function Home() {
   return (
-    <>
+    <div className="center">
       <Head>
         <title>HONGZHEN CHEN Page</title>
       </Head>
-      <h1>HONGZHEN CHEN Page
-      </h1>
-      <p>
-        This is a sample page for HONGZHEN CHEN
-      </p>
-      <Link href="/about">About</Link>
-    </>
-  )
+      <h1>HONGZHEN CHEN Page</h1>
+      <div>
+        <LoginButton />
+      </div>
+      <p>This is a sample page for HONGZHEN CHEN.</p>
+
+      <Link href="/about">About</Link> |
+      <Link href="/blogs">Blogs</Link>
+    </div>
+  );
 }
